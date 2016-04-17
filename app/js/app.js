d314b92a5fe7fd62638c320dc2a1e8e3b4954df4
Palak Lal <palaklal18@gmail.com>
2016-04-17 00:21:13 -0400

    instead of using example request body for HTTP call to QPX API, it now uses the search form's data

--- app/js/app.js
+++ app/js/app.js
@@ -2,32 +2,35 @@
 	var app = angular.module('lalAirlines', []);
 
 	var results = []; //array to store all the flights matching user's search criteria
-	app.controller('searchController', [ '$http', function($http){ //controller for search form
+	app.controller('searchController', [ '$http', '$filter', function($http, $filter){ //controller for search form
 		this.form = {};
-
 		this.searchFlights = (function(){
+			//convert HTML dates to date format the API requires
+			this.form.startDate = $filter('date')(this.form.startDate, 'yyyy-MM-dd');
+			this.form.endDate = $filter('date')(this.form.endDate, 'yyyy-MM-dd');
+
 			console.log(this.form);
 			var request = { //request body for http.post to Google Flights QPX API
 			  request: {
 			    passengers: {
-			      adultCount: 1
+			      adultCount: this.form.num
 			    },
 			    slice: [
 			      {
-			        origin: "BOS",
-			        destination: "DEL",
-			        date: "2016-04-20"
+			        origin: this.form.origin,
+			        destination: this.form.destination,
+			        date: this.form.startDate + ""
 			      },
 			      {
-			        origin: "DEL",
-			        destination: "BOS",
-			        date: "2016-04-24"
+			        origin: this.form.destination,
+			        destination: this.form.origin,
+			        date: this.form.endDate + ""
 			      }
 			    ],
-			    solutions: 5
+			    maxPrice: "USD" + this.form.max
 			  }
 			};
-			//console.log(request);
+			console.log(request);
 			$http({ //http post to Google QPX API to receive all the flights that match user's search criteria
 				url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=API_KEY',
 				method: 'POST',
