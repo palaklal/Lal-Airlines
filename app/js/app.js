940c5f87a5ef053535ca9fb7795bcec9a4442448
Palak Lal <palaklal18@gmail.com>
2016-04-16 20:33:51 -0400

    added google flights qpxExpress API call

--- app/js/app.js
+++ app/js/app.js
@@ -2,12 +2,49 @@
 	var app = angular.module('lalAirlines', []);
 
 	var results = [];
-	app.controller('searchController', function(){
+	app.controller('searchController', [ '$http', function($http){
 		this.form = {};
 
 		this.searchFlights = (function(){
 			console.log(this.form);
+			var request = {
+			  request: {
+			    passengers: {
+			      adultCount: 1
+			    },
+			    slice: [
+			      {
+			        origin: "BOS",
+			        destination: "LAX",
+			        date: "2016-04-20"
+			      },
+			      {
+			        origin: "LAX",
+			        destination: "BOS",
+			        date: "2016-04-20"
+			      }
+			    ],
+			    solutions: 5
+			  }
+			};
+			console.log(request);
+			$http({
+				url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=API_KEY',
+				method: 'POST',
+				data: request,
+				headers: {'Content-Type': 'application/json'}	
+			}).then(function(response) {
+				console.log("SUCCESS");
+				console.log(response.data.trips);
+			}, function(response) {
+				console.log("ERROR "+response.status);
+			});
+
 			this.form = {};
 		});
+	}]);
+
+	app.controller('flightsController', function(){
+		this.empty = false;
 	});
 })();
