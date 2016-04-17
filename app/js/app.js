66e598346965852692bcdcd751299c27b7271cf9
Palak Lal <palaklal18@gmail.com>
2016-04-16 23:47:22 -0400

    added functionality for parsing through Google Flights QPX HTTP response for all relevant data

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,13 @@
 (function() {
 	var app = angular.module('lalAirlines', []);
 
-	var results = [];
-	app.controller('searchController', [ '$http', function($http){
+	var results = []; //array to store all the flights matching user's search criteria
+	app.controller('searchController', [ '$http', function($http){ //controller for search form
 		this.form = {};
 
 		this.searchFlights = (function(){
 			console.log(this.form);
-			var request = {
+			var request = { //request body for http.post to Google Flights QPX API
 			  request: {
 			    passengers: {
 			      adultCount: 1
@@ -15,36 +15,65 @@
 			    slice: [
 			      {
 			        origin: "BOS",
-			        destination: "LAX",
+			        destination: "DEL",
 			        date: "2016-04-20"
 			      },
 			      {
-			        origin: "LAX",
+			        origin: "DEL",
 			        destination: "BOS",
-			        date: "2016-04-20"
+			        date: "2016-04-24"
 			      }
 			    ],
 			    solutions: 5
 			  }
 			};
-			console.log(request);
-			$http({
+			//console.log(request);
+			$http({ //http post to Google QPX API to receive all the flights that match user's search criteria
 				url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=API_KEY',
 				method: 'POST',
 				data: request,
 				headers: {'Content-Type': 'application/json'}	
 			}).then(function(response) {
 				console.log("SUCCESS");
-				console.log(response.data.trips);
-			}, function(response) {
-				console.log("ERROR "+response.status);
-			});
+				var flights = response.data.trips.tripOption;
+				//console.log(flights);
+				for (var flight in flights) { //iterate through every possible itinerary 
+					if (flights.hasOwnProperty(flight)) {
+						var itinerary = { }; //data structure for each itinerary to push onto forms
+						itinerary.price = flights[flight].pricing[0].saleTotal;
+						itinerary.roundtrip = [];
+						for (var f = 0; f < flights[flight].slice.length; f++) {
+							var oneWay = []; //from ORIGIN to DESTINATION flight
+							for (var s = 0; s < flights[flight].slice[f].segment.length; s++) { //each leg of that flight
+								var segment = flights[flight].slice[f].segment[s]; 
 
+								var stop = {};
+								stop.carrier = segment.flight.carrier;
+								stop.flightNumber = segment.flight.number;
+								stop.departureTime = segment.leg[0].departureTime;
+								stop.arrivalTime = segment.leg[0].arrivalTime;
+								stop.origin = segment.leg[0].origin;
+								stop.destination = segment.leg[0].destination;
+								oneWay.push(stop);
+							}
+						itinerary.roundtrip.push(oneWay);
+						}
+					}
+				results.push(itinerary);
+				console.log(results);
+				}
+			}, function(response) {
+				console.log("ERROR: "+response.status);
+			});
+			console.log(results);
 			this.form = {};
 		});
 	}]);
 
 	app.controller('flightsController', function(){
-		this.empty = false;
+		this.empty = true;
+		if (results.length) {
+			this.empty = false;
+		}
 	});
 })();
