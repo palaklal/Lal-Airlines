20b3d41aa66fb18f2ad5b83c0b5b2893f1a7739f
Palak Lal <palaklal18@gmail.com>
2016-04-17 23:25:18 -0400

    flights that match search criteria display below search form now after button is clicked

--- app/js/app.js
+++ app/js/app.js
@@ -1,14 +1,13 @@
 (function() {
 	var app = angular.module('lalAirlines', []);
-
-	var results = []; //array to store all the flights matching user's search criteria
-	app.controller('searchController', [ '$http', '$filter', function($http, $filter){ //controller for search form
+	results = [];
+	app.controller('searchController', [ '$http', '$filter', function($http, $filter){ //controller for search form and results
 		this.form = {};
+		this.results = []; //array to store all the flights matching user's search criteria
 		this.searchFlights = (function(){
 			//convert HTML dates to date format the API requires
 			this.form.startDate = $filter('date')(this.form.startDate, 'yyyy-MM-dd');
 
-			console.log(this.form);
 			var request = { //request body for http.post to Google Flights QPX API
 			  request: {
 			    passengers: {
@@ -20,8 +19,7 @@
 			        destination: this.form.destination,
 			        date: this.form.startDate + ""
 			      }
-			    ],
-			    solutions: 10
+			    ]
 			  }
 			};
 			if (this.form.endDate) { //this isn't a required field so check if it's been flled out first and then add to request
@@ -36,21 +34,19 @@
 			if (this.form.max) { //if user set a maximum ticket price, add to request
 				request.request.maxPrice = "USD" + this.form.max;
 			}
-			console.log(request);
 
 			$http({ //http post to Google QPX API to receive all the flights that match user's search criteria
-				url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=YOUR_API',
+				url: 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=API_KEY',
 				method: 'POST',
 				data: request,
 				headers: {'Content-Type': 'application/json'}	
 			}).then(function(response) {
 				console.log("SUCCESS");
 				var flights = response.data.trips.tripOption;
-				//console.log(flights);
 				for (var flight in flights) { //iterate through every possible itinerary 
 					if (flights.hasOwnProperty(flight)) {
 						var itinerary = { }; //data structure for each itinerary to push onto forms
-						itinerary.price = flights[flight].pricing[0].saleTotal;
+						itinerary.price = "Price: " + flights[flight].pricing[0].saleTotal;
 						itinerary.roundtrip = [];
 						for (var f = 0; f < flights[flight].slice.length; f++) {
 							var oneWay = []; //from ORIGIN to DESTINATION flight
@@ -66,24 +62,23 @@
 								stop.destination = segment.leg[0].destination;
 								oneWay.push(stop);
 							}
+						oneWay.startDate = flights[flight].slice[f].segment[0].leg[0].departureTime; //start date for entire one-way journey
+						oneWay.arrivalDate = flights[flight].slice[f].segment[flights[flight].slice[f].segment.length - 1].leg[0].arrivalTime; //arrival date for entire one-way journey
+						oneWay.numberOfStops = flights[flight].slice[f].segment.length;
 						itinerary.roundtrip.push(oneWay);
 						}
 					}
 				results.push(itinerary);
-				console.log(results);
 				}
 			}, function(response) {
 				console.log("ERROR: "+response.status);
+				//trick to force error back to frontend
+				var error = {};
+				error.price = "There was an error in your request! Please try again later!";
+				results.push(error);
 			});
-			console.log(results);
-			this.form = {};
+			this.results = results;
+			this.form = {}; //reset form
 		});
 	}]);
-
-	app.controller('flightsController', function(){
-		this.empty = true;
-		if (results.length) {
-			this.empty = false;
-		}
-	});
 })();
